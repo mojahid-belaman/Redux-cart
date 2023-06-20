055d82816bca58d1cc904f2f06ee4eb48f66d223
mojahid-belaman <mojahidbelaman@gmail.com>
2023-06-20 16:58:24 +0100

    get state from redux and dispatch action when click add product

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,9 +1,20 @@
-import Card from '../UI/Card';
-import classes from './ProductItem.module.css';
+import { useDispatch } from "react-redux";
+import Card from "../UI/Card";
+import classes from "./ProductItem.module.css";
+import { addProduct } from "../../store/product";
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
-
+  const dispatch = useDispatch();
+  const { id, title, price, description } = props.product;
+  const clickHandler = () => {
+    const prodObj = {
+      id,
+      title,
+      price,
+      amount: 1,
+    };
+    dispatch(addProduct(prodObj));
+  };
   return (
     <li className={classes.item}>
       <Card>
@@ -13,7 +24,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={clickHandler}>Add to Cart</button>
         </div>
       </Card>
     </li>
