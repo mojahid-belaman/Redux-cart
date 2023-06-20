0c9ef073d95013f9cc4a256976d2c70f6a905a96
mojahid-belaman <mojahidbelaman@gmail.com>
2023-06-20 16:59:01 +0100

    map from every product

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,16 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import { useSelector } from "react-redux";
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
 
 const Products = (props) => {
+  const products = useSelector((state) => state.products.products);
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {products.map((elem) => (
+          <ProductItem key={elem.id} product={elem} />
+        ))}
       </ul>
     </section>
   );
