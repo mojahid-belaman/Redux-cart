79ca0ac8d25c7afae8a2c5a097ff5385a5fd4739
mojahid-belaman <mojahidbelaman@gmail.com>
2023-06-20 16:59:33 +0100

    create slice product

---
+++ src/store/product.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  products: [
+    {
+      id: `p_${Math.random()}`,
+      title: "Test",
+      description: "This is first product-amazing!",
+      price: 6,
+      amount: 1,
+    },
+  ],
+};
+
+const productSlice = createSlice({
+  name: "product",
+  initialState,
+  reducers: {
+    addProduct(state, action) {
+      state.products.push(action.payload);
+    },
+  },
+});
+
+export const { addProduct } = productSlice.actions;
+
+export default productSlice.reducer;
