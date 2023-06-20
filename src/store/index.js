7f76eb8789483ce1dc866b74c5543b306a58a416
mojahid-belaman <mojahidbelaman@gmail.com>
2023-06-20 17:00:07 +0100

    create store and add all reducers

---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import productReducer from "./product";
+
+const store = configureStore({
+  reducer: { products: productReducer },
+});
+
+export default store;
