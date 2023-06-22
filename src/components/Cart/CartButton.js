276b0d6e015708b6d456bbe0c3c317c79a16bcd9
mojahid-belaman <mojahidbelaman@gmail.com>
2023-06-22 09:36:27 +0100

    dispatch action to reducer to handle visibility the cart

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,15 @@
+import { useDispatch } from 'react-redux';
 import classes from './CartButton.module.css';
+import { toggle } from '../../store/ui';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+
+  const toggleHandler = () => {
+    dispatch(toggle())
+  }
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
