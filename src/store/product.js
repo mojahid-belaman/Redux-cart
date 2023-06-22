7315c17c6f3b015f9b55205e98e57e6923620351
mojahid-belaman <mojahidbelaman@gmail.com>
2023-06-22 19:53:56 +0100

    add new action to store data in redux

--- src/store/product.js
+++ src/store/product.js
@@ -9,6 +9,10 @@ const productSlice = createSlice({
   name: "product",
   initialState,
   reducers: {
+    replaceProduct(state, action) {
+      state.products = action.payload.products;
+      state.totalQte = action.payload.totalQte;
+    },
     addProduct(state, action) {
       const item = action.payload;
       const checkItem = state.products.find((elem) => elem.id === item.id);
@@ -29,7 +33,7 @@ const productSlice = createSlice({
     removeProduct(state, action) {
       const itemId = action.payload;
       const existItem = state.products.find((elem) => elem.id === itemId);
-      state.totalQte--
+      state.totalQte--;
       if (existItem.quantity > 1) {
         existItem.quantity--;
         existItem.totalPrice = existItem.totalPrice - existItem.price;
@@ -41,6 +45,7 @@ const productSlice = createSlice({
   },
 });
 
-export const { addProduct, removeProduct } = productSlice.actions;
+export const { addProduct, removeProduct, replaceProduct } =
+  productSlice.actions;
 
 export default productSlice;
