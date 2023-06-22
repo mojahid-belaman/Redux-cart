369b25a87a77064dc19d2cf524bdc65c120ecb0b
mojahid-belaman <mojahidbelaman@gmail.com>
2023-06-22 13:19:34 +0100

    get totalQte from redux and display it to ui

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,17 +1,18 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './CartButton.module.css';
 import { toggle } from '../../store/ui';
 
 const CartButton = (props) => {
+  const totalQte = useSelector(state => state.products.totalQte)
   const dispatch = useDispatch();
-
+  
   const toggleHandler = () => {
     dispatch(toggle())
   }
   return (
     <button className={classes.button} onClick={toggleHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{totalQte}</span>
     </button>
   );
 };
