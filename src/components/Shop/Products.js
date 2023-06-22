df4029a0c0749fa3d121f4f7967c9220aa0740b8
mojahid-belaman <mojahidbelaman@gmail.com>
2023-06-22 19:54:52 +0100

    create dummy data about detail of product

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,63 +1,35 @@
-import { useEffect, useState } from "react";
 import ProductItem from "./ProductItem";
 import classes from "./Products.module.css";
 import Card from "../UI/Card";
 
+const DUMMY_DATA = [
+  {
+    id: "p1",
+    title: "First",
+    price: 29,
+    description: "This is a first product - amazing",
+  },
+  {
+    id: "p2",
+    title: "Second",
+    price: 30,
+    description: "This is a second product.",
+  },
+];
+
 const Products = (props) => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchProduct = async () => {
-      setLoading(true);
-      setError(null);
-      try {
-        const res = await fetch(
-          "https://reduxcart-cb3e1-default-rtdb.firebaseio.com/products.json"
-        );
-        if (!res.ok) throw new Error("Somthing Went Wrong!");
-        const data = await res.json();
-        let prodArr = [];
-        for (const key in data) {
-          const item = {
-            id: key,
-            title: data[key].title,
-            price: data[key].price,
-            description: data[key].description,
-          };
-          prodArr.push(item);
-        }
-        setProducts(prodArr);
-        setLoading(false);
-      } catch (error) {
-        setLoading(false);
-        setError(error.message);
-      }
-    };
-    fetchProduct();
-  }, []);
-
-  let content = <p>No Founds Products!</p>;
-  if (products.length > 0) {
-    content = (
-      <ul>
-        {products.map((elem) => (
-          <ProductItem key={elem.id} product={elem} />
-        ))}
-      </ul>
-    );
-  }
-  if (loading) {
-    content = <p>LOADING...</p>;
-  }
-  if (error) {
-    content = <p>{error}</p>;
-  }
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
-      <Card>{content}</Card>
+      <Card>
+        {
+          <ul>
+            {DUMMY_DATA.map((elem) => (
+              <ProductItem key={elem.id} product={elem} />
+            ))}
+          </ul>
+        }
+      </Card>
     </section>
   );
 };
