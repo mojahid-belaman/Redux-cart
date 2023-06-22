fd876c4f1f8ee34fcc254435bcb0e66f7de5427b
mojahid-belaman <mojahidbelaman@gmail.com>
2023-06-22 13:20:12 +0100

    get data cart from redux

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,18 @@
-import Card from '../UI/Card';
-import classes from './Cart.module.css';
-import CartItem from './CartItem';
+import { useSelector } from "react-redux";
+import Card from "../UI/Card";
+import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
+  const items = useSelector((state) => state.products.products);
+
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
+        {items.map((elem) => (
+          <CartItem key={elem.id} item={elem} />
+        ))}
       </ul>
     </Card>
   );
