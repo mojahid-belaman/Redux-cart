77fdfc972ecf9db79cfe094bd58ee4f61c04506f
mojahid-belaman <mojahidbelaman@gmail.com>
2023-06-22 13:18:24 +0100

    add two function add and remove after that bind it to button

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,14 +1,30 @@
-import classes from './CartItem.module.css';
+import { useDispatch } from "react-redux";
+import classes from "./CartItem.module.css";
+import { addProduct, removeProduct } from "../../store/product";
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const dispatch = useDispatch();
+  const { id, title, quantity, totalPrice, price } = props.item;
 
+  const addHandler = () => {
+    dispatch(
+      addProduct({
+        id,
+        title,
+        price,
+      })
+    );
+  };
+  const removeHandler = () => {
+    console.log(id);
+    dispatch(removeProduct(id));
+  };
   return (
     <li className={classes.item}>
       <header>
         <h3>{title}</h3>
         <div className={classes.price}>
-          ${total.toFixed(2)}{' '}
+          ${totalPrice.toFixed(2)}{" "}
           <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
         </div>
       </header>
@@ -17,8 +33,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeHandler}>-</button>
+          <button onClick={addHandler}>+</button>
         </div>
       </div>
     </li>
