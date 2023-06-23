6a7284f4b0a1d98ffae1d88d840a7f8a48cd2832
mojahid-belaman <mojahidbelaman@gmail.com>
2023-06-23 17:16:20 +0100

    call logic fetch data from redux inside useEffect and display notification

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { Fragment, useEffect } from "react";
+import Notification from "./components/UI/Notification";
+import { fetchProductData, sendDataProduct } from "./store/product-action";
+
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisisble);
+  const products = useSelector((state) => state.products);
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchProductData())
+  }, [dispatch])
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    if (products.changed) {
+      dispatch(sendDataProduct(products));
+    }
+  }, [products, dispatch]);
+
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && <Notification notification={notification} />}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
