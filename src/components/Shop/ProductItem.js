39544df5fd54db3f7e5a8ad54d530b9e4782f45e
mojahid-belaman <mojahidbelaman@gmail.com>
2023-06-23 17:14:42 +0100

    cut logic fetch data with redux from inside component to store

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,32 +1,11 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import Card from "../UI/Card";
 import classes from "./ProductItem.module.css";
 import { addProduct } from "../../store/product";
-import { useEffect } from "react";
 
 const ProductItem = (props) => {
-  const products = useSelector((state) => state.products);
   const dispatch = useDispatch();
   const { id, title, price, description } = props.product;
-
-  useEffect(() => {
-    const sendRequest = async () => {
-      try {
-        const res = await fetch(
-          "https://reduxcart-cb3e1-default-rtdb.firebaseio.com/products.json",
-          {
-            method: "PUT",
-            body: JSON.stringify(products),
-          }
-        );
-        const data = res.json();
-        console.log(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    sendRequest();
-  }, [products]);
 
   const clickHandler = () => {
     const prodObj = {
